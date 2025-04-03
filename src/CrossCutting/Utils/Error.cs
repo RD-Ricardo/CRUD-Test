3d47264173937144f5fb887ce67d21851c414315
RD-Ricardo <ricardodias169@gmail.com>
2025-04-02 22:42:34 -0300

    feat: utils

---
+++ src/CrossCutting/Utils/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace CrossCutting.Utils
+{
+    public class Error
+    {
+        public string Message { get; set; }
+        public ErrorTypeEnum Type { get; set; }
+        public Error(string message, ErrorTypeEnum type)
+        {
+            Message = message;
+            Type = type;
+        }
+    }
+}
---
+++ src/CrossCutting/Utils/Result.cs
@@ -0,0 +1,44 @@
+﻿namespace CrossCutting.Utils
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; }
+        public Error[]? Errors { get; set; }
+
+        public static Result<T> Success(T Data)
+        {
+            return new Result<T>
+            {
+                IsSuccess = true,
+                Data = Data
+            };
+        }
+
+        public static Result<T> Failure(Error error)
+        {
+            return new Result<T>
+            {
+                IsSuccess = false,
+                Errors = [error]
+            };
+        }
+
+        public static Result<T> Failure(Error[] errors)
+        {
+            return new Result<T>
+            {
+                IsSuccess = false,
+                Errors = errors
+            };
+        }
+    }
+
+    public enum ErrorTypeEnum
+    {
+        NotFound = 0,
+        BadRequest = 1,
+        Validation = 2,
+        InternalServerError = 3
+    }
+}
