6f27117608653765a1b9aea5ff036697097ff940
RD-Ricardo <ricardodias169@gmail.com>
2025-04-03 00:51:31 -0300

    feat: profile address

---
+++ src/Application/Mappers/AddressProfile.cs
@@ -0,0 +1,23 @@
+﻿using Application.Addresses.Commands.CreateAddress;
+using Application.Addresses.Commands.UpdateAddress;
+using Application.Addresses.Models;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappers
+{
+    public class AddressProfile : Profile
+    {
+        public AddressProfile()
+        {
+            CreateMap<CreateAddressCommand, Address>()
+                .ForMember(dest => dest.CustomerId, opt => opt.Ignore())
+                .ForMember(dest => dest.Customer, opt => opt.Ignore());
+
+            CreateMap<UpdateAddressCommand, Address>();
+
+            CreateMap<Address, AddressReponse>()
+                .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Customer.Name));
+        }
+    }
+}
